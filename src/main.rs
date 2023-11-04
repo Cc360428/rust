6bbb8d767f5c5396bacb2bc7ca52a0bbfbddb752
Cc <li_chao_cheng@163.com>
2023-11-04 16:36:27 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::io;
 use rand::Rng;
-// 3
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("{}", "猜数游戏！");
-    println!("猜测一个游戏");
-    let secret_number = rand::thread_rng().gen_range(1,101);
-    println!("{:?}",secret_number);
+    println!("猜数游戏！");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("神秘数字是：{}", secret_number);
+    println!("才一个数字");
     let mut guess = String::new();
+
     io::stdin().read_line(&mut guess).expect("无法读取行");
+
+    let guess: u32 = guess.trim().parse().expect("转换失败");
+
     println!("你猜测测的数是：{}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too samll"),
+        Ordering::Greater => println!("To big"),
+        Ordering::Equal => println!("You Win"),
+    }
 }
