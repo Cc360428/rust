cd5e7df7df8e26cbde35d5b7f269ac4a686c6cb9
Cc <li_chao_cheng@163.com>
2023-12-13 21:19:21 +0800

    wb

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,65 @@
-// #[derive(Debug)]
-enum Element {
-    Integer(i32),
-    Int(i32),
-    Float(f64),
-    Text(String),
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
 }
-fn main() {
-    let mut v_new: Vec<Element> = Vec::new();
-    v_new.push(Element::Integer(10));
-    v_new.push(Element::Int(100));
-    v_new.push(Element::Float(10.11));
-    v_new.push(Element::Text(String::from("Super")));
 
-    for element in &v_new {
-        match element {
-            Element::Integer(value) => println!("{}", value),
-            Element::Int(value) => println!("{}", value),
-            Element::Float(value) => println!("{}", value),
-            Element::Text(value) => println!("{}", value),
-        }
+#[post("/gcd")]
+async fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
     }
 
-    // println!("{:?}", v_new);
-    let v_t = vec![
-        Element::Integer(1),
-        Element::Int(100),
-        Element::Float(10.11),
-        Element::Text(String::from("Super")),
-    ];
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} \
+                is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
 
-    for element in &v_t {
-        match element {
-            Element::Integer(value) => println!("{}", value),
-            Element::Int(value) => println!("{}", value),
-            Element::Float(value) => println!("{}", value),
-            Element::Text(value) => println!("{}", value),
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+async fn get_index() -> impl Responder {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+    )
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0, "两个数字都为零！");
+    while m != 0 {
+        if m < n {
+            let t: u64 = m;
+            m = n;
+            n = t;
         }
+        m = m % n;
     }
-    // println!("{:?}", v_t)
+    n
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(post_gcd)
+            .route("/", web::get().to(get_index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
